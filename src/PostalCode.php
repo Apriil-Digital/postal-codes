850532a4435eb39c4c138a57cc58cb0aafa95587
Thomas Alrek <thomas@alrek.no>
2026-05-20 13:48:17 +0200

    Enhance PostalCode model by adding a protected array for rows with sample data

--- src/PostalCode.php
+++ src/PostalCode.php
@@ -13,6 +13,10 @@ class PostalCode extends Model
     public $incrementing = false;
     protected $keyType = 'string';
 
+    protected $casts = [
+        'id' => 'string',
+    ];
+
     protected array $rows = [
         ['id' => '0001', 'name' => 'Oslo'],
         ['id' => '0010', 'name' => 'Oslo'],
